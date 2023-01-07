25a9bbec47c6d6faa3de7544b0a4c95d57b04582
Hexzel <a0972180845@gmail.com>
2023-01-07 19:24:32 +0800

    modified grayworld as grayworld2

---
+++ UWIE/matlab/gray_world2.m
@@ -0,0 +1,7 @@
+function output = gray_world2(I)
+    I_lin = rgb2lin(I);
+    percentiles = 5;
+    illuminant = illumgray(I_lin, percentiles);
+    I_lin = chromadapt(I_lin,illuminant, 'ColorSpace', 'linear-rgb');
+    output = lin2rgb(I_lin);
+end
